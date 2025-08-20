a0cbd8bde37c65e1455aff2327e516816ea7304c
Naveenkumar <naveenkumarchapala686@gmail.com>
2025-08-20 20:31:56 +0530

    job role

--- api.py
+++ api.py
@@ -22,10 +22,25 @@ class base_model(BaseModel):
 def root():
     return 'hey welcome'
 
-@app.post('/predict')
+@app.post('/pre')
 def predict_re(item:base_model):
     value=item.Resume
+    value=re.sub('[^a-z A-Z 0-9]','',value)
+    ##remove stopwords
+    value=" ".join([ wor for wor in value.split() if wor not in stopwords.words('english')])
+    
+    ##remove html tags
+    value=BeautifulSoup(value,'lxml').get_text()
+    #remove links
+    value=re.sub(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?','',str(value))
+    ##remove spaces
+    value=" ".join(value.split())
+    ##apply wordlematizing
+   
+    value=" ".join(WordNetLemmatizer().lemmatize(i) for i in value.split())
+    
+
     
     vector_predict=vector.transform([value])
     model_predict_valuer=model.predict(vector_predict)
-    return (f'News is:',str(model_predict_valuer[0]))
+    return (f'Job Role:',str(model_predict_valuer[0]))
